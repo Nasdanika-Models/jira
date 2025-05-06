c2819a35184e4032a17a8e90b53f672bdd8fef58
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2025-05-06 08:22:53 -0400

    Drawio class diagram

--- processors/src/main/java/org/nasdanika/models/jira/processors/EcoreGenJiraProcessorsFactory.java
+++ processors/src/main/java/org/nasdanika/models/jira/processors/EcoreGenJiraProcessorsFactory.java
@@ -1,13 +1,38 @@
 package org.nasdanika.models.jira.processors;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.util.Collection;
+import java.util.Map;
 import java.util.function.BiConsumer;
 
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EClassifier;
+import org.eclipse.emf.ecore.EDataType;
+import org.eclipse.emf.ecore.EEnum;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
 import org.nasdanika.common.Context;
+import org.nasdanika.common.NasdanikaException;
 import org.nasdanika.common.ProgressMonitor;
+import org.nasdanika.drawio.Connection;
+import org.nasdanika.drawio.Document;
+import org.nasdanika.drawio.Layer;
+import org.nasdanika.drawio.Model;
+import org.nasdanika.drawio.Node;
+import org.nasdanika.drawio.Page;
+import org.nasdanika.drawio.Root;
+import org.nasdanika.graph.emf.EObjectNode;
 import org.nasdanika.graph.processor.NodeProcessorConfig;
 import org.nasdanika.models.app.Action;
 import org.nasdanika.models.app.Label;
 import org.nasdanika.models.app.graph.WidgetFactory;
+import org.nasdanika.models.ecore.graph.processors.EClassNodeProcessor;
+import org.nasdanika.models.ecore.graph.processors.EClassifierNodeProcessor;
+import org.nasdanika.models.ecore.graph.processors.EClassifierNodeProcessorFactory;
+import org.nasdanika.models.ecore.graph.processors.EDataTypeNodeProcessor;
+import org.nasdanika.models.ecore.graph.processors.EEnumNodeProcessor;
 import org.nasdanika.models.ecore.graph.processors.EPackageNodeProcessor;
 import org.nasdanika.models.ecore.graph.processors.EPackageNodeProcessorFactory;
 import org.nasdanika.models.jira.JiraPackage;
@@ -45,6 +70,12 @@ public class EcoreGenJiraProcessorsFactory {
 
 				```drawio-resource
 				jira.drawio
+				```				
+				
+				## Class diagram
+				
+				```drawio-resource
+				jira-classes.drawio
 				```				
 												
 				## Using client
@@ -219,8 +250,136 @@ public class EcoreGenJiraProcessorsFactory {
 					labelConfigurator.accept(label, progressMonitor);
 				}
 			}
+						
+//			/**
+//			 * Generating a Drawio diagram
+//			 */
+//			@Override
+//			protected void generateDiagramAndGraphActions(Collection<Label> labels, ProgressMonitor progressMonitor) {
+//				super.generateDiagramAndGraphActions(labels, progressMonitor);
+//				
+//				try {
+//					Document document = Document.create(false, null);
+//					Page page = document.createPage();
+//					page.setName("Jira");
+//					
+//					Model model = page.getModel();
+//					Root root = model.getRoot();
+//					Layer backgroundLayer = root.getLayers().get(0);
+////					Map<EPackage, Layer> layers = new HashMap<>();
+//					
+//					generateDrawioDiagram(
+////						ePackage -> layers.computeIfAbsent(ePackage, ep -> {
+////							Layer layer = root.createLayer();
+////							layer.setLabel(ep.getName());
+////							return layer;
+////						}),
+//						ep -> backgroundLayer,	
+//						false, 
+//						false, 
+//						progressMonitor);
+//					
+//					org.nasdanika.drawio.Util.forceLayout(root, 1920 * 1.5, 1080 * 1.5);
+//												
+//					Files.writeString(new File("target/jira-classes.drawio").toPath(), document.save(null));
+//				} catch (Exception e) {
+//					e.printStackTrace();
+//					throw new NasdanikaException(e);
+//				}								
+//			}			
 			
 		};
 	}	
-
+		
+//	@EClassifierNodeProcessorFactory
+//	public EClassifierNodeProcessor<?> createEClassifierProcessor(
+//			NodeProcessorConfig<WidgetFactory, WidgetFactory> config, 
+//			java.util.function.Function<ProgressMonitor, Action> prototypeProvider,
+//			BiConsumer<Label, ProgressMonitor> labelConfigurator,
+//			ProgressMonitor progressMonitor) {
+//		
+//		EObject eClassifier = ((EObjectNode) config.getElement()).get();
+//				
+//		if (eClassifier instanceof EClass) {
+//			return new EClassNodeProcessor(config, context, prototypeProvider) {
+//				
+//				@Override
+//				public void configureLabel(Object source, Label label, ProgressMonitor progressMonitor) {
+//					super.configureLabel(source, label, progressMonitor);
+//					if (labelConfigurator != null) {
+//						labelConfigurator.accept(label, progressMonitor);
+//					}
+//				}	
+//				
+//				@Override
+//				protected EList<? super Action> getMembersActionCollection(Action parent) {
+//					return parent.getChildren();
+//				}
+//				
+//				@Override
+//				protected EList<? super Action> getMembersCollection(Action membersAction) {
+//					return membersAction.getChildren();
+//				}
+//				
+//				@Override
+//				protected void addDiagramAction(Action action, Action diagramAction) {
+//					action.getSections().add(diagramAction);
+//				}
+//				
+//				@Override
+//				protected int getDiagramNodeWidth() {
+//					return Math.max(getTarget().getName().length() * 5, super.getDiagramNodeWidth());
+//				}
+//				
+//				@Override
+//				protected void createDrawioConnection(
+//						Layer layer, 
+//						EClassifierNodeProcessor<?> dependency,
+//						Node diagramNode, 
+//						Node targetNode) {
+//					
+//					EClassifier targetEClassifier = dependency.getTarget();
+//					
+//					// Supertype
+//					if (getTarget().getESuperTypes().contains(targetEClassifier)) {
+//						// TODO - connect top center of the sub-class to the bottom center of super-class
+//						Connection inheritance = layer.createConnection(diagramNode, targetNode);
+//						Map<String, String> style = inheritance.getStyle();
+//						style.put("edgeStyle", "orthogonalEdgeStyle");
+//						style.put("rounded", "0");
+//						style.put("orthogonalLoop", "1");
+//						style.put("jettySize", "auto");
+//						style.put("html", "1");
+//						style.put("endArrow", "block");
+//						style.put("endFill", "0");
+//					}
+//					
+//					// Reference
+//					for (EReference ref: getTarget().getEReferences()) {
+//						if (ref.getEType() == targetEClassifier) {
+//							Connection containment = layer.createConnection(diagramNode, targetNode);
+//							containment.setLabel(ref.getName());
+//							Map<String, String> style = containment.getStyle();
+//							style.put("rounded", "0");
+//							style.put("orthogonalLoop", "1");
+//							style.put("jettySize", "1");
+//							style.put("html", "1");
+//							if (ref.isMany()) {
+//								style.put("startArrow", "diamondThin");
+//								style.put("startFill", "1");
+//							}
+//						}
+//					}
+//				}
+//				
+//			};
+//		}
+//		
+//		if (eClassifier instanceof EEnum) {
+//			return new EEnumNodeProcessor(config, context, prototypeProvider);
+//		}
+//		
+//		return new EDataTypeNodeProcessor<EDataType>(config, context, prototypeProvider);		
+//	}	
+	
 }
